cfae8f1e1ebc245c1037ebc4ee4c665707cf3e9a
nemoshi-dayo <050112yuan@gmail.com>
2024-11-18 23:38:47 +0900

    all commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()]
+    integrations: [tailwind()],
+    site: 'https://nemoshi-dayo.github.io',
+    base: '/Dynamic-Ratio-Tuner'
 });
 
 
